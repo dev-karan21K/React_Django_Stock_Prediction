bf5596568042569e1a7c506e6411d4cea32ba0a7
dev-karan21K <kn5271443@gmail.com>
2025-03-21 13:07:50 +0530

    home page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,15 @@
 
+import './assets/css/style.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer '>
+        <hr className='border' />
+        <p className='text-light text-center'> &copy; 2025 - Built with ❤️ by Karan</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
